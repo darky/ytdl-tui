5a1fb297a121376f9d1c6e932a9dc940cfca512e
Vladislav Botvin <darkvlados@gmail.com>
2022-12-26 23:33:32 +0300

    sugarify download ns with p-event

--- src/download.ts
+++ src/download.ts
@@ -8,6 +8,7 @@ import type { State } from 'src/types'
 import { ns } from 'repl-ns'
 import { durationNS } from 'src/duration'
 import ffmpegPath from 'ffmpeg-static'
+import pEvent from 'p-event'
 
 if (ffmpegPath) process.env['FFMPEG_PATH'] = ffmpegPath
 
@@ -18,39 +19,31 @@ export const downloadNS = ns('download', {
     try {
       const temporaryFilePath = await downloadNS().createTempFilePath()
 
-      downloadNS()
-        .youtubeDownload(state.url)
-        .on('error', err => downloadNS().renderErr(err))
-        .pipe(
-          downloadNS()
-            .writeTempFile(temporaryFilePath)
-            .on('error', err => downloadNS().renderErr(err))
-        )
-        .on('finish', async () => {
-          try {
-            if (state.startTime || state.endTime || state.resolution !== 'highest') {
-              let ffmpegStream = downloadNS().createFfmpeg(temporaryFilePath)
-              if (state.startTime) {
-                ffmpegStream = ffmpegStream.setStartTime(state.startTime)
-              }
-              if (state.endTime) {
-                ffmpegStream = ffmpegStream.setDuration(durationNS().calcDuration(state))
-              }
-              if (state.resolution !== 'highest') {
-                ffmpegStream = ffmpegStream.size(`?x${state.resolution}`)
-              }
-              ffmpegStream
-                .saveToFile(state.path)
-                .on('error', err => downloadNS().renderErr(err))
-                .on('end', () => downloadNS().renderComplete())
-            } else {
-              await downloadNS().cpFile(temporaryFilePath, state.path)
-              downloadNS().renderComplete()
-            }
-          } catch (err) {
-            downloadNS().renderErr(err as Error)
-          }
-        })
+      await pEvent(
+        downloadNS()
+          .youtubeDownload(state.url)
+          .on('error', downloadNS().renderErr)
+          .pipe(downloadNS().writeTempFile(temporaryFilePath).on('error', downloadNS().renderErr)),
+        'finish'
+      )
+
+      if (state.startTime || state.endTime || state.resolution !== 'highest') {
+        let ffmpegStream = downloadNS().createFfmpeg(temporaryFilePath)
+        if (state.startTime) {
+          ffmpegStream = ffmpegStream.setStartTime(state.startTime)
+        }
+        if (state.endTime) {
+          ffmpegStream = ffmpegStream.setDuration(durationNS().calcDuration(state))
+        }
+        if (state.resolution !== 'highest') {
+          ffmpegStream = ffmpegStream.size(`?x${state.resolution}`)
+        }
+        await pEvent(ffmpegStream.saveToFile(state.path), 'end')
+      } else {
+        await downloadNS().cpFile(temporaryFilePath, state.path)
+      }
+
+      downloadNS().renderComplete()
     } catch (err) {
       downloadNS().renderErr(err as Error)
     }
